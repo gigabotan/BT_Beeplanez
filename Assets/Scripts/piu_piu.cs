f0425196f2a7b22c805ca1c876a60d91ad91e549
minibotan <minibotan1996@gmail.com>
2015-02-18 14:52:52 +0300

    сделал второго игрока
    сделал второго игрока, но картикок все еще нет

--- Assets/Scripts/piu_piu.cs
+++ Assets/Scripts/piu_piu.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class piu_piu : MonoBehaviour {
-
+	private float speed = 80f;
 	// Use this for initialization
+
+	public void left(){
+			speed = -80f;
+	}
+
 	void Start () {
 		Destroy (this.gameObject, 2f); //уничтожает эту пульку, если она летит более двух секунд
 	}
 	// Update is called once per frame
 	void Update () {
 		// перемещение пульки вперед
-		transform.position += transform.right * 80f * Time.deltaTime;
+		transform.position += transform.right * Time.deltaTime*speed;
 
 		// если пулька врезалась в землю или улетела за пределы карты, то уничтожается почти мгновенно
 		if (transform.position.y < -13f || Mathf.Abs(transform.position.x) > 40f ) {
--- Assets/Scripts/planeController2.cs
+++ Assets/Scripts/planeController2.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class planeController : MonoBehaviour {
-
+public class planeController2 : MonoBehaviour {
+	
 	private float maxspeed = 30f; // скорость
 	private float speeddecrease = -.005f; // уменьшение скорость
 	private float turnspeed = 10f; // скорость поворота
@@ -15,28 +15,28 @@ public class planeController : MonoBehaviour {
 	private float attspeed = 1; // скорострельность
 	private float atttime = 1; // когда была выпущена последняя пулька
 	public GameObject spwn; // точка, откуда вылетают пульки
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		respawn();
 	}
-
+	
 	void respawn() {// респавнит самолетик
 		death++; // увеличивает количество смертей
-		this.transform.position = new Vector3 (-21f, -13f, 0f); // начальная позиция
+		this.transform.position = new Vector3 (21f, -13f, 0f); // начальная позиция
 		this.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f)); // начальный угол
 		currSpeed = 0f; // сбрасывает скорость
 		onGround = true; // говорит, что самолетик на земле, чтоб он не взорвался снова
 		health = 5f; // количество жизней пополняется до максимума
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D coll) { // если самелетик врезался в землю
 		if (!onGround && coll.gameObject.tag != "plane") { // если уже взлетел, то true
 			respawn();  // происходит респаун
 		}
 	}
-
+	
 	void OnTriggerEnter2D (Collider2D col) { // если встретился с триггером (пульки, например)
 		if (col.tag == "piu_piu") { // если это пулька
 			health--; // отнимаем один хп
@@ -47,50 +47,51 @@ public class planeController : MonoBehaviour {
 			respawn(); // респаун
 		}
 	}
-
+	
 	// Update is called once per frame
 	void Update () { 
-
-		// тут прописан поворот самолетика, зависит от текущей скорости
-		transform.rotation *= Quaternion.Euler(0f, 0f,(-Input.GetAxis("P1hor")*turnspeed*(.2f + (currSpeed*2/5))));
-
-
-		if (transform.position.y < 20f) {  // если самолетик улетел слишком высоко, то мы теряем управление
-			currSpeed += Input.GetAxis ("P1vert") * Time.deltaTime; // тут прописано увеличение скорости
-		}
-			currSpeed += speeddecrease; // тут постоянное уменьшение скорости
-			currSpeed = Mathf.Max (0, currSpeed); // проверка на то, чтоб скорость не была слишком маленькой
-			currSpeed = Mathf.Min (1, currSpeed); // проверка на то, чтоб скорость не была слишком высокой
 		
-
-
+		// тут прописан поворот самолетика, зависит от текущей скорости
+		transform.rotation *= Quaternion.Euler(0f, 0f,(-Input.GetAxis("P2hor")*turnspeed*(.2f + (currSpeed*2/5))));
+		
+		
+		if (transform.position.y < 20f) {  // если самолетик улетел слишком высоко, то мы теряем управление
+			currSpeed += Input.GetAxis ("P2vert") * Time.deltaTime; // тут прописано увеличение скорости
+		}
+		currSpeed += speeddecrease; // тут постоянное уменьшение скорости
+		currSpeed = Mathf.Max (0, currSpeed); // проверка на то, чтоб скорость не была слишком маленькой
+		currSpeed = Mathf.Min (1, currSpeed); // проверка на то, чтоб скорость не была слишком высокой
+		
+		
+		
 		if (transform.position.y > -12f) // если самолетик взлетел, то говорим, что он уже не на земле.
-						onGround = false;
-
-
-		transform.position += transform.right * currSpeed* Time.deltaTime* maxspeed; // тут перемещаем вамолетик вперед
-
+			onGround = false;
+		
+		
+		transform.position -= transform.right * currSpeed* Time.deltaTime* maxspeed; // тут перемещаем вамолетик вперед
+		
 		if (!onGround) { // если самолет уже взлетел, то на него начинает действовать гравитация
 			transform.position += (daunV * Time.deltaTime) / (currSpeed * .9f + .9f);
+			
+		}
 		
-		}
-
-
+		
 		if (Mathf.Abs (transform.position.x) > 40f) { //если самолетик вылетел за пределы карты, то он появится с другой стороны
-			transform.position = new Vector3((-39f/40f*transform.position.x),transform.position.y,transform.position.z);		
+			transform.position = new Vector3(-(transform.position.x),transform.position.y,transform.position.z);		
 		}
-
-
+		
+		
 		// тут прописана атака и скорострельность
-		if (!onGround && Input.GetKey (KeyCode.Space) && ((Time.time - atttime) * (attspeed + 3f)) > 2f) {
-			Instantiate(piu, spwn.transform.position, this.transform.rotation);
+		if (!onGround && Input.GetKey (KeyCode.RightControl) && ((Time.time - atttime) * (attspeed + 3f)) > 2f) {
+			GameObject tmp = Instantiate(piu, spwn.transform.position, this.transform.rotation) as GameObject;
+			tmp.SendMessage("left");
 			atttime = Time.time;
 		}
 	}
-
-
+	
+	
 	void FixedUpdate() {
-
-
+		
+		
 	}
 }
